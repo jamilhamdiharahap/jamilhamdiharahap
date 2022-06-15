ac9bd3d1f63b2ef8cd1a0f062fa82dcb5c282dc4
jamilhamdiharahap <jamilhamdihrp@gmail.com>
2022-06-15 14:15:58 +0700

    pelatihan react

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html", "**/*.md", "**/*.yml", "**/*.json", "**/*.js", "**/*.css", "**/*.scss", "**/*.ts", "**/*.tsx", "**/*.vue", "**/*.jsx", "**/*.mdx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
